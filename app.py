cfad5260ee3ebb3fa0df8702718122e8803fb525
Shawn Lee <thisisshawnlee@gmail.com>
2023-02-27 09:06:37 +0800

    add player name, photo and team

--- app.py
+++ app.py
@@ -3,17 +3,21 @@ import plotly.express as px
 import pandas as pd
 
 from nba_api.stats.static.players import get_players
+from nba_api.stats.static.teams import get_teams
 from nba_api.stats.endpoints import playercareerstats
 from nba_api.stats.endpoints import playerdashboardbyyearoveryear
 
 players = pd.DataFrame(get_players())
 players = players.set_index('id')
+teams = pd.DataFrame(get_teams())
+teams = teams.set_index('id')
 
 # Show LeBron James when page first loads
 player = 'LeBron James'
 player_id = players[players['full_name'] == player].index[0]
 player_stats = playerdashboardbyyearoveryear.PlayerDashboardByYearOverYear(player_id).get_data_frames()[1]
 seasons_played = player_stats['GROUP_VALUE']
+team = teams.at[player_stats.at[0, 'TEAM_ID'], 'full_name']
 games_played = player_stats.at[0, 'GP']
 points_per_game = player_stats.at[0, 'PTS'] / games_played
 rebounds_per_game = player_stats.at[0, 'REB'] / games_played
@@ -38,7 +42,15 @@ app.layout = html.Div(children=[
                 dcc.Dropdown(seasons_played, seasons_played[0], id='season-select'),
             ], className='dropdown-labeled')
             
-        ]),  
+        ]),
+        
+        html.Div(id='player-info', children=[
+            html.Div(children=[
+                html.P(id='player-team', children=team),
+                html.H1(id='player-name', children=player), 
+            ]),
+            html.Img(id='player-headshot', src=f'https://cdn.nba.com/headshots/nba/latest/1040x760/{player_id}.png'),
+        ]),
 
         html.Div(id='summary-stats', children=[
             html.Div(children=[
@@ -59,6 +71,9 @@ app.layout = html.Div(children=[
 @app.callback(
     Output('season-select', 'options'),
     Output('season-select', 'value'),
+    Output('player-headshot', 'src'),
+    Output('player-name', 'children'),
+    Output('player-team', 'children'),
     Output('points', 'children'),
     Output('rebounds', 'children'),
     Output('assists', 'children'),
@@ -74,13 +89,17 @@ def update_dashboard(player, season):
     else:
         season_index = 0
         season = seasons_played[0]
+        
+    team = teams.at[player_stats.at[season_index, 'TEAM_ID'], 'full_name']
+        
+    headshot_src = f'https://cdn.nba.com/headshots/nba/latest/1040x760/{player_id}.png'
     
     games_played = player_stats.at[season_index, 'GP']
     points_per_game = player_stats.at[season_index, 'PTS'] / games_played
     rebounds_per_game = player_stats.at[season_index, 'REB'] / games_played
     assists_per_game = player_stats.at[season_index, 'AST'] / games_played
         
-    return seasons_played, season, f'PPG: {points_per_game:.0f}', f'RPG: {rebounds_per_game:.0f}', f'APG: {assists_per_game:.0f}'
+    return seasons_played, season, headshot_src, player, team, f'PPG: {points_per_game:.0f}', f'RPG: {rebounds_per_game:.0f}', f'APG: {assists_per_game:.0f}'
 
 
 if __name__ == '__main__':
