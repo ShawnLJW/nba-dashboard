b616ce9f65df766a973b651e1df42815b1d3ce5d
Shawn Lee <thisisshawnlee@gmail.com>
2023-03-02 15:59:28 +0800

    redraw court for shot chart

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import plotly.graph_objects as go
+import numpy as np
 import pandas as pd
 
 from nba_api.stats.static.players import get_players
@@ -12,6 +13,99 @@ players = pd.DataFrame(get_players())
 players = players.set_index('id')
 teams = pd.DataFrame(get_teams())
 teams = teams.set_index('id')
+
+def ellipse_arc(x_center=0.0, y_center=0.0, a=10.5, b=10.5, start_angle=0.0, end_angle=2 * np.pi, N=200, closed=False):
+    t = np.linspace(start_angle, end_angle, N)
+    x = x_center + a * np.cos(t)
+    y = y_center + b * np.sin(t)
+    path = f'M {x[0]}, {y[0]}'
+    for k in range(1, len(t)):
+        path += f'L{x[k]}, {y[k]}'
+    if closed:
+        path += ' Z'
+    return path
+
+court = [   
+    # paint
+    dict(
+        type="rect", x0=-80, y0=-47.5, x1=80, y1=137.5, layer='below'
+    ),
+    dict(
+        type="rect", x0=-60, y0=-47.5, x1=60, y1=137.5, layer='below'
+    ),
+    
+    # Free throw circle
+    dict(
+        type="circle", x0=-60, y0=77.5, x1=60, y1=197.5, xref="x", yref="y", layer='below'
+    ),
+    dict(
+        type="line", x0=-60, y0=137.5, x1=60, y1=137.5, layer='below'
+    ),
+
+    # rim
+    dict(
+        type="circle", x0=-7.5, y0=-7.5, x1=7.5, y1=7.5, xref="x", yref="y",
+        line=dict(color="#ec7607", width=2),
+    ),
+    
+    #backboard
+    dict(
+        type="line", x0=-30, y0=-7.5, x1=30, y1=-7.5
+    ),
+    
+    # three point line
+    dict(
+        type="path", path=ellipse_arc(a=237.5, b=237.5, start_angle=0.386283101, end_angle=np.pi - 0.386283101), layer='below'
+    ),
+    dict(
+        type="line", x0=-220, y0=-47.5, x1=-220, y1=92.5, layer='below'
+    ),
+    dict(
+        type="line", x0=220, y0=-47.5, x1=220, y1=92.5, layer='below'
+    ),
+    dict(
+        type="line", x0=-250, y0=227.5, x1=-220, y1=227.5, layer='below'
+    ),
+    dict(
+        type="line", x0=250, y0=227.5, x1=220, y1=227.5, layer='below'
+    ),
+    
+    # hash marks
+    dict(
+        type="line", x0=-90, y0=17.5, x1=-80, y1=17.5, layer='below'
+    ),
+    dict(
+        type="line", x0=-90, y0=27.5, x1=-80, y1=27.5, layer='below'
+    ),
+    dict(
+        type="line", x0=-90, y0=57.5, x1=-80, y1=57.5, layer='below'
+    ),
+    dict(
+        type="line", x0=-90, y0=87.5, x1=-80, y1=87.5, layer='below'
+    ),
+    dict(
+        type="line", x0=90, y0=17.5, x1=80, y1=17.5, layer='below'
+    ),
+    dict(
+        type="line", x0=90, y0=27.5, x1=80, y1=27.5, layer='below'
+    ),
+    dict(
+        type="line", x0=90, y0=57.5, x1=80, y1=57.5, layer='below'
+    ),
+    dict(
+        type="line", x0=90, y0=87.5, x1=80, y1=87.5, layer='below'
+    ),
+    
+    # No foul line
+    dict(
+        type="path", path=ellipse_arc(a=40, b=40, start_angle=0, end_angle=np.pi), layer='below'
+    ),
+
+    # Center circle
+    dict(
+        type="path", path=ellipse_arc(y_center=417.5, a=60, b=60, start_angle=-0, end_angle=-np.pi), layer='below'
+    ),
+]
 
 # Show LeBron James when page first loads
 player = 'LeBron James'
@@ -30,19 +124,11 @@ assists_per_game = player_stats.at[0, 'AST'] / games_played
 def plot_shots(shots):
     fig = px.scatter(
         x=shots['LOC_X'], y=shots['LOC_Y'],
-        width=540, height=490
+        width=540, height=510
     )
-    fig.update_layout(margin={'l':20, 'r':20, 't':20, 'b':20}, plot_bgcolor="#e3b871")
+    fig.update_layout(margin={'l':20, 'r':20, 't':20, 'b':20}, plot_bgcolor="#e3b871", shapes=court)
     fig.update_xaxes(range=[-250, 250], visible=False)
-    fig.update_yaxes(range=[-50, 425], visible=False)
-    
-    fig.add_shape(type='rect', x0=-250, x1=250, y0=-47.5, y1=422.5)
-    fig.add_shape(type='rect', x0=-80, x1=80, y0=-47.5, y1=142.5)
-    fig.add_shape(type='line',x0=-220, x1=-220, y0=-47.5, y1=92.5)
-    fig.add_shape(type='line',x0=220, x1=220, y0=-47.5, y1=92.5)
-    fig.add_shape(type='circle', x0=-237.5, x1=237.5, y0=-237.5, y1=237.5)
-    fig.add_shape(type='line',x0=-30, x1=30, y0=-7.5, y1=-7.5)
-    fig.add_shape(type='circle', x0=-7.5, x1=7.5, y0=-7.5, y1=7.5, line={'color':'#ec5f2f'})
+    fig.update_yaxes(range=[-47.5, 422.5], visible=False)
     return fig
 
 app = Dash(__name__)
@@ -93,7 +179,6 @@ app.layout = html.Div(children=[
     ]),
 
 ])
-
 
 @app.callback(
     Output('season-select', 'options'),
